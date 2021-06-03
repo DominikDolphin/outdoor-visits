76216ac88f423557023eae57144c3fc9f2298971
dominik thibaudeau <dominik.thibaudeau@gmail.com>
2021-06-03 11:57:39 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -2,26 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 import Clock from './Clock';
+import {Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Hello />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <Hello />
+    //     <Clock />
+    //   </header>
+    // </div>
+    
+    <Switch>
+      <Route exact path='/'>
         <Clock />
-      </header>
-    </div>
+      </Route>
+      <Route exact path='/Hello'>
+        <Hello />
+      </Route>
+    </Switch>
   );
 }
 
